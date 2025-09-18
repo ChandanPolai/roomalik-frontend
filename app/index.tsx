1c00cedd979a984d49b5aa630390f482a0d682da
ChandanPolai <chandanpolai26@gmail.com>
2025-09-19 01:52:45 +0530

    frontend project setup

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css"
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-500 ">
+      <Text className="text-xl font-bold text-blue-800">
+        Welcome to Nativewind! this is chandan polaieefefefefef
+      </Text>
+    </View>
+  )
+}
+
+export default index
